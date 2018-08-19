1e6a4ff89288a7651306fe0d2b0856f36211c7fa
Bilal <elkhattouti.b@gmail.com>
2018-08-19 12:26:54 +0200

    Model v1

---
+++ Models/Cours.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+	[Table("Cours")]
+	public class Cours
+	{
+		[Key]
+		public int IdCours { get; set; }
+		[Required]
+		public string Nom { get; set; }
+		[Required]
+		public bool Annulee { get; set; }
+		public virtual ObservableCollection<Etudiant> Inscrits { get; set; }
+		public virtual ObservableCollection<Etudiant> Tuteurs { get; set; }
+	}
+}
---
+++ Models/Etudiant.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Models
+{
+	public class Etudiant
+	{
+		[Key]
+		public int IdEtudiant { get; set; }
+		[Required, MinLength(5), MaxLength(5), Index(IsUnique = true)]
+		public string PSR { get; set; }
+		[Required]
+		public string Nom { get; set; }
+		[Required]
+		public string Prenom { get; set; }
+		[Required]
+		public bool Desinscrit { get; set; }
+
+		public virtual ObservableCollection<Cours> Cours { get; set; }
+		public virtual ObservableCollection<Cours> Tuteurs { get; set; }
+	}
+}
